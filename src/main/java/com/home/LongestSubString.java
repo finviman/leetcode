879bc0231e8c1923cea5ff857c0f6ea181671932
liyang70 <liyang70@meituan.com>
2020-05-30 14:14:10 +0800

    format with coc

--- src/main/java/com/home/LongestSubString.java
+++ src/main/java/com/home/LongestSubString.java
@@ -1,40 +1,39 @@
 package com.home;
 
 /**
- * Hello world!
- * Longest Substring Without Repeating Characters
+ * Hello world! Longest Substring Without Repeating Characters
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
  */
 public class LongestSubString {
 
-    public static void main( String[] args ) {
-        System.out.println("final result "+new LongestSubString().lengthOfLongestSubstring(""));
+    public static void main(String[] args) {
+        System.out.println("final result " + new LongestSubString().lengthOfLongestSubstring(""));
     }
 
     public int lengthOfLongestSubstring(String s) {
 
-        int maxLength=0;
+        int maxLength = 0;
         char[] array = s.toCharArray();
         int length = s.length();
-        for(int i=0;i<length;i++) {
-            int currentPhaseLength=0;
-            for(int j=i+1;j<length;j++) {
-                if (isContain(array[j],array, i, j)) {
+        for (int i = 0; i < length; i++) {
+            int currentPhaseLength = 0;
+            for (int j = i + 1; j < length; j++) {
+                if (isContain(array[j], array, i, j)) {
                     break;
-                }else{
+                } else {
                     currentPhaseLength++;
                 }
             }
-            if (currentPhaseLength+1>=maxLength) {
-                maxLength=currentPhaseLength+1;
+            if (currentPhaseLength + 1 >= maxLength) {
+                maxLength = currentPhaseLength + 1;
             }
         }
         return maxLength;
     }
 
-    private boolean isContain(char c, char[] array,int start,int end) {
-        for(int i=start;i<end;i++) {
-            if (c==array[i]) {
+    private boolean isContain(char c, char[] array, int start, int end) {
+        for (int i = start; i < end; i++) {
+            if (c == array[i]) {
                 return true;
             }
         }

80b3fa73b2533661b359f68197476fa0e7ba43cb
liyang70 <liyang70@meituan.com>
2021-06-10 00:57:22 +0800

    in hangzhou

---
+++ src/main/java/com/home/MaxProfit.java
@@ -0,0 +1,23 @@
+package com.home;
+
+public class MaxProfit {
+    public static void main(String[] args){
+        int[] prices = {7,1,5,3,6,4};
+        int max = maxProfit(prices);
+        System.out.println(max);
+    }
+
+    private int maxProfit(int[] prices){
+        int mini = prices[0];
+        int maxProfit = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>mini){
+                if(prices[i]-mini>maxProfit)
+                    maxProfit = prices[i]-mini;
+            }else{
+                mini = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}

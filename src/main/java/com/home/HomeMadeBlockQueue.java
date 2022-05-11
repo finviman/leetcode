4f8539740a1426f2ccb7dc4ac411fb1c19d3c493
liyang <li.acerphoenix@gmail.com>
2022-05-11 23:53:18 +0800

    success function and test
    comment
    
    ready

--- src/main/java/com/home/HomeMadeBlockQueue.java
+++ src/main/java/com/home/HomeMadeBlockQueue.java
@@ -21,32 +21,35 @@ public class HomeMadeBlockQueue<T>{
 
     public void put(T element) throws InterruptedException {
         synchronized (oneLock) {
-            if (currentSize==CAPACITY) {//满了
+            //生产线程持有锁后开始执行
+            while (currentSize == CAPACITY) {
+                //符合条件就wait，停止执行进入锁的等待队列，
+                //会释放锁，其他线程就可以执行了，等其他线程notify后从这里继续执行
                 oneLock.wait();
-            }else {
-                elements[prodPos] = element;
-                prodPos = (prodPos+1) & (CAPACITY - 1);
-                currentSize++;
-                System.out.println("put element:"+element+ " and size="+currentSize);
-                oneLock.notifyAll();
             }
+            elements[prodPos] = element;
+            prodPos = (prodPos+1) & (CAPACITY - 1);
+            currentSize++;
+            System.out.println("put element:"+element+ " and size="+currentSize);
+            //执行到这里，就要用notify释放锁，
+            //然后通知其他wait的线程(进入锁等待队列的线程)出队，进入ready状态
+            //cpu会调度正在ready的线程。
+            oneLock.notify();
         }
     }
 
     public T get() throws InterruptedException {
         synchronized (oneLock) {
-            if (currentSize == 0) {
+            while(currentSize == 0) {
                 oneLock.wait();
-            }else {
-                T element = (T) elements[consumePos];
-                consumePos = (consumePos+1) & (CAPACITY - 1);
-                currentSize --;
-                System.out.println("get elemment:"+element + " and size="+currentSize);
-                oneLock.notifyAll();
-                return element;
             }
+            T element = (T) elements[consumePos];
+            consumePos = (consumePos+1) & (CAPACITY - 1);
+            currentSize --;
+            System.out.println("get elemment:"+element + " and size="+currentSize);
+            oneLock.notify();
+            return element;
         }
-        return null;
     }
 
     public static void main(String[] args) throws InterruptedException {
@@ -66,7 +69,7 @@ public class HomeMadeBlockQueue<T>{
         Runnable c =()->{
             while (true){
                 try {
-                    Thread.sleep(1700);
+                    Thread.sleep(171);
                     bq.get();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
